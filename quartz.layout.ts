03ba8d73999ce35359c9ebbeb0ad85463cc9db1c
zenodotus280 <16273022+zenodotus280@users.noreply.github.com>
2024-09-23 21:09:41 -0700

    fix: resolve build failure after 'npx quartz update'
    added 'afterBody: [],' to quartz.layout.ts to continue the build

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -5,6 +5,7 @@ import * as Component from "./quartz/components"
 export const sharedPageComponents: SharedLayout = {
   head: Component.Head(),
   header: [Component.TagList()],
+  afterBody: [],
   footer: Component.Footer({
     links: {
       'Website (GitHub)': "https://github.com/zenodotus280/apl",
