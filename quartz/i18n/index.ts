a53772bbd1dd2804872ae0e519bedeb2a2fd4287
Osman Tunahan ARIKAN <154083945+OsmanTunahan@users.noreply.github.com>
2024-10-31 01:33:22 +0300

    feat(i18n): add turkish language (#1554)

--- quartz/i18n/index.ts
+++ quartz/i18n/index.ts
@@ -20,6 +20,7 @@ import hu from "./locales/hu-HU"
 import fa from "./locales/fa-IR"
 import pl from "./locales/pl-PL"
 import cs from "./locales/cs-CZ"
+import tr from "./locales/tr-TR"
 
 export const TRANSLATIONS = {
   "en-US": enUs,
@@ -64,6 +65,7 @@ export const TRANSLATIONS = {
   "fa-IR": fa,
   "pl-PL": pl,
   "cs-CZ": cs,
+  "tr-TR": tr,
 } as const
 
 export const defaultTranslation = "en-US"
