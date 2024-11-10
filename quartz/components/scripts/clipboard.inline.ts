1ab9c91df1df799f7a1004200ff890b144d00f19
Aaron Pham <contact@aarnphm.xyz>
2024-11-10 18:13:12 -0500

    feat(mermaid): improvement navigation (#1575)
    * feat(mermaid): custom stuff
    
    Signed-off-by: Aaron Pham <contact@aarnphm.xyz>
    
    * chore: use mermaid and update clipboard content
    
    Signed-off-by: Aaron Pham <contact@aarnphm.xyz>
    
    * fix: explicitly use center div
    
    Signed-off-by: Aaron Pham <contact@aarnphm.xyz>
    
    ---------
    
    Signed-off-by: Aaron Pham <contact@aarnphm.xyz>

--- quartz/components/scripts/clipboard.inline.ts
+++ quartz/components/scripts/clipboard.inline.ts
@@ -8,7 +8,9 @@ document.addEventListener("nav", () => {
   for (let i = 0; i < els.length; i++) {
     const codeBlock = els[i].getElementsByTagName("code")[0]
     if (codeBlock) {
-      const source = codeBlock.innerText.replace(/\n\n/g, "\n")
+      const source = (
+        codeBlock.dataset.clipboard ? JSON.parse(codeBlock.dataset.clipboard) : codeBlock.innerText
+      ).replace(/\n\n/g, "\n")
       const button = document.createElement("button")
       button.className = "clipboard-button"
       button.type = "button"
