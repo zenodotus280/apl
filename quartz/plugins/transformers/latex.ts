9188939b1f29acd0c330e00e96c0ed73b326e2cd
Han-Miru Kim <hanmiru.kim@gmail.com>
2024-11-12 12:42:44 +0100

    feat(math): typst support(closes #1568) (#1569)
    Co-authored-by: Aaron Pham <contact@aarnphm.xyz>
    Co-authored-by: Aaron Pham <Aaronpham0103@gmail.com>

--- quartz/plugins/transformers/latex.ts
+++ quartz/plugins/transformers/latex.ts
@@ -1,11 +1,20 @@
 import remarkMath from "remark-math"
 import rehypeKatex from "rehype-katex"
 import rehypeMathjax from "rehype-mathjax/svg"
+//@ts-ignore
+import rehypeTypst from "@myriaddreamin/rehype-typst"
 import { QuartzTransformerPlugin } from "../types"
+import { KatexOptions } from "katex"
+import { Options as MathjaxOptions } from "rehype-mathjax/svg"
+//@ts-ignore
+import { Options as TypstOptions } from "@myriaddreamin/rehype-typst"
 
 interface Options {
-  renderEngine: "katex" | "mathjax"
+  renderEngine: "katex" | "mathjax" | "typst"
   customMacros: MacroType
+  katexOptions: Omit<KatexOptions, "macros" | "output">
+  mathJaxOptions: Omit<MathjaxOptions, "macros">
+  typstOptions: TypstOptions
 }
 
 interface MacroType {
@@ -21,32 +30,37 @@ export const Latex: QuartzTransformerPlugin<Partial<Options>> = (opts) => {
       return [remarkMath]
     },
     htmlPlugins() {
-      if (engine === "katex") {
-        return [[rehypeKatex, { output: "html", macros }]]
-      } else {
-        return [[rehypeMathjax, { macros }]]
+      switch (engine) {
+        case "katex": {
+          return [[rehypeKatex, { output: "html", macros, ...(opts?.katexOptions ?? {}) }]]
+        }
+        case "typst": {
+          return [[rehypeTypst, opts?.typstOptions ?? {}]]
+        }
+        case "mathjax": {
+          return [[rehypeMathjax, { macros, ...(opts?.mathJaxOptions ?? {}) }]]
+        }
+        default: {
+          return [[rehypeMathjax, { macros, ...(opts?.mathJaxOptions ?? {}) }]]
+        }
       }
     },
     externalResources() {
-      if (engine === "katex") {
-        return {
-          css: [
-            {
-              // base css
-              content: "https://cdnjs.cloudflare.com/ajax/libs/KaTeX/0.16.9/katex.min.css",
-            },
-          ],
-          js: [
-            {
-              // fix copy behaviour: https://github.com/KaTeX/KaTeX/blob/main/contrib/copy-tex/README.md
-              src: "https://cdnjs.cloudflare.com/ajax/libs/KaTeX/0.16.9/contrib/copy-tex.min.js",
-              loadTime: "afterDOMReady",
-              contentType: "external",
-            },
-          ],
-        }
-      } else {
-        return {}
+      switch (engine) {
+        case "katex":
+          return {
+            css: [{ content: "https://cdn.jsdelivr.net/npm/katex@0.16.11/dist/katex.min.css" }],
+            js: [
+              {
+                // fix copy behaviour: https://github.com/KaTeX/KaTeX/blob/main/contrib/copy-tex/README.md
+                src: "https://cdn.jsdelivr.net/npm/katex@0.16.11/dist/contrib/copy-tex.min.js",
+                loadTime: "afterDOMReady",
+                contentType: "external",
+              },
+            ],
+          }
+        default:
+          return { css: [], js: [] }
       }
     },
   }
