965f9e123ca8f6bd5342bf9fe8daaa3e3a58ee2e
Aaron Pham <contact@aarnphm.xyz>
2024-12-23 15:00:26 -0500

    chore(frontmatter): dealias created/modified/updated time (#1664)

--- quartz/plugins/transformers/lastmod.ts
+++ quartz/plugins/transformers/lastmod.ts
@@ -48,11 +48,9 @@ export const CreatedModifiedDate: QuartzTransformerPlugin<Partial<Options>> = (u
                 created ||= st.birthtimeMs
                 modified ||= st.mtimeMs
               } else if (source === "frontmatter" && file.data.frontmatter) {
-                created ||= file.data.frontmatter.date as MaybeDate
-                modified ||= file.data.frontmatter.lastmod as MaybeDate
-                modified ||= file.data.frontmatter.updated as MaybeDate
-                modified ||= file.data.frontmatter["last-modified"] as MaybeDate
-                published ||= file.data.frontmatter.publishDate as MaybeDate
+                created ||= file.data.frontmatter.created as MaybeDate
+                modified ||= file.data.frontmatter.modified as MaybeDate
+                published ||= file.data.frontmatter.published as MaybeDate
               } else if (source === "git") {
                 if (!repo) {
                   // Get a reference to the main git repo.
